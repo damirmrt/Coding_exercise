b979384fa3a6950269d68318db9160d394023398
damirmrt <damirmrt@gmail.com>
2023-06-12 18:58:51 +0400

    added discription

--- Codewars/3kyu_ Make_a_Spiral.py
+++ Codewars/3kyu_ Make_a_Spiral.py
@@ -1,9 +1,17 @@
 """
+https://www.codewars.com/kata/534e01fbbb17187c7e0000c6
+
+Main idea in my solution is using four pointer. One pointer for each side of 2D array.
+On first cycle you iterate ove outer edge of array,
+then increment/decrement all pointers by 1 and repeat till you reach center.
+
 
 
 """
+
+
 def spiralize(size):
-    res = [[0 ] *size for i in range(size)]
+    res = [[0] * size for i in range(size)]
     l = 0
     r = size
     t = 0
@@ -11,19 +19,19 @@ def spiralize(size):
 
     while t < b:
 
-        for i in range(l ,r):
+        for i in range(l, r):
             res[t][i] = 1
 
-        for i in range( t +1 ,b):
-            res[i][ r -1] = 1
+        for i in range(t + 1, b):
+            res[i][r - 1] = 1
 
         t += 2
         if l > 0:
             l += 1
 
-        if siz e /2 + 1 == b:
+        if size / 2 + 1 == b:
             return res
-        for i in range( r -2, l- 1, -1):
+        for i in range(r - 2, l - 1, -1):
             res[b - 1][i] = 1
 
         for i in range(b - 1, t - 1, -1):
@@ -34,8 +42,3 @@ def spiralize(size):
         l += 1
 
     return res
-
-
-
-
-
